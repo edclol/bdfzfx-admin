209a3db820f3bbe774e0a1f2ab33710d5f2f9a8b
zhongke <1163836763@qq.com>
2025-09-01 21:05:53 +0800

    无用信息隐藏

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -55,9 +55,9 @@
       </el-form-item>
     </el-form>
     <!--  底部  -->
-    <div class="el-login-footer">
+    <!-- <div class="el-login-footer">
       <span>Copyright © 2018-2025 All Rights Reserved.</span>
-    </div>
+    </div> -->
   </div>
 </template>
 
