9aafab5c70e1e17dfbcbe312e6be1ed496decaf3
edclol <jessepinkmansmail@gmail.com>
2025-12-16 18:10:50 +0800

    默认不拦截

--- bdfzfx-framework/src/main/java/com/bdfzfx/framework/config/SSOConfig.java
+++ bdfzfx-framework/src/main/java/com/bdfzfx/framework/config/SSOConfig.java
@@ -2,6 +2,7 @@ package com.bdfzfx.framework.config;
 
 import com.sgcc.isc.ualogin.client.filter.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * SSO 认证配置类
  */
 @Configuration
+@ConditionalOnProperty(name = "isc.sso.enabled", havingValue = "true")
 public class SSOConfig {
 
     // 从配置文件读取参数
