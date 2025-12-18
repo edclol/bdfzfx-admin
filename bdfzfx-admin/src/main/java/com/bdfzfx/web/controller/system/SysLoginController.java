819c2864d541041bbbba62fd1a329534d786a2f2
edclol <jessepinkmansmail@gmail.com>
2025-12-18 16:12:44 +0800

    feat(cas-callback): 添加登录成功状态展示

--- bdfzfx-admin/src/main/java/com/bdfzfx/web/controller/system/SysLoginController.java
+++ bdfzfx-admin/src/main/java/com/bdfzfx/web/controller/system/SysLoginController.java
@@ -1,27 +1,7 @@
 package com.bdfzfx.web.controller.system;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-
-import com.bdfzfx.common.exception.user.UserNotExistsException;
-import com.bdfzfx.system.service.ISysUserService;
-import com.sgcc.isc.ualogin.client.IscServiceTicketValidator;
-import com.sgcc.isc.ualogin.client.util.IscSSOResourceUtil;
-import com.sgcc.isc.ualogin.client.vo.IscSSOUserBean;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.jasig.cas.client.javafilter.validation.TicketValidationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.bdfzfx.common.constant.Constants;
 import com.bdfzfx.common.core.domain.AjaxResult;
 import com.bdfzfx.common.core.domain.entity.SysMenu;
@@ -29,6 +9,7 @@ import com.bdfzfx.common.core.domain.entity.SysUser;
 import com.bdfzfx.common.core.domain.model.LoginBody;
 import com.bdfzfx.common.core.domain.model.LoginUser;
 import com.bdfzfx.common.core.text.Convert;
+import com.bdfzfx.common.exception.user.UserNotExistsException;
 import com.bdfzfx.common.utils.DateUtils;
 import com.bdfzfx.common.utils.SecurityUtils;
 import com.bdfzfx.common.utils.StringUtils;
@@ -37,24 +18,36 @@ import com.bdfzfx.framework.web.service.SysPermissionService;
 import com.bdfzfx.framework.web.service.TokenService;
 import com.bdfzfx.system.service.ISysConfigService;
 import com.bdfzfx.system.service.ISysMenuService;
+import com.bdfzfx.system.service.ISysUserService;
+import com.sgcc.isc.ualogin.client.IscServiceTicketValidator;
+import com.sgcc.isc.ualogin.client.util.IscSSOResourceUtil;
+import com.sgcc.isc.ualogin.client.vo.IscSSOUserBean;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-
-// 添加FastJSON2的导入
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 登录验证
- * 
+ *
  *
  */
 @Api(tags = "登录验证")
 @RestController
-public class SysLoginController
-{
+public class SysLoginController {
     private static final Logger logger = LoggerFactory.getLogger(SysLoginController.class);
-    
+
     @Autowired
     private SysLoginService loginService;
 
@@ -81,16 +74,16 @@ public class SysLoginController
 
     @Autowired
     private ISysUserService userService;
+
     /**
      * 用户名密码登录方法
-     * 
+     *
      * @param loginBody 登录信息
      * @return 结果
      */
     @PostMapping("/login")
     @ApiOperation("用户名密码登录")
-    public AjaxResult login(@RequestBody LoginBody loginBody)
-    {
+    public AjaxResult login(@RequestBody LoginBody loginBody) {
         AjaxResult ajax = AjaxResult.success();
         // 生成令牌
         String token = loginService.login(loginBody.getUsername(), loginBody.getPassword(), loginBody.getCode(),
@@ -101,7 +94,7 @@ public class SysLoginController
 
     /**
      * SSO登录方法
-     * 
+     *
      * @return 结果
      */
     @GetMapping("/isc/callback")
@@ -186,21 +179,19 @@ public class SysLoginController
 
     /**
      * 获取用户信息
-     * 
+     *
      * @return 用户信息
      */
     @GetMapping("getInfo")
     @ApiOperation("获取用户信息")
-    public AjaxResult getInfo()
-    {
+    public AjaxResult getInfo() {
         LoginUser loginUser = SecurityUtils.getLoginUser();
         SysUser user = loginUser.getUser();
         // 角色集合
         Set<String> roles = permissionService.getRolePermission(user);
         // 权限集合
         Set<String> permissions = permissionService.getMenuPermission(user);
-        if (!loginUser.getPermissions().equals(permissions))
-        {
+        if (!loginUser.getPermissions().equals(permissions)) {
             loginUser.setPermissions(permissions);
             tokenService.refreshToken(loginUser);
         }
@@ -215,33 +206,28 @@ public class SysLoginController
 
     /**
      * 获取路由信息
-     * 
+     *
      * @return 路由信息
      */
     @GetMapping("getRouters")
     @ApiOperation("获取路由信息")
-    public AjaxResult getRouters()
-    {
+    public AjaxResult getRouters() {
         Long userId = SecurityUtils.getUserId();
         List<SysMenu> menus = menuService.selectMenuTreeByUserId(userId);
         return AjaxResult.success(menuService.buildMenus(menus));
     }
-    
+
     // 检查初始密码是否提醒修改
-    public boolean initPasswordIsModify(Date pwdUpdateDate)
-    {
+    public boolean initPasswordIsModify(Date pwdUpdateDate) {
         Integer initPasswordModify = Convert.toInt(configService.selectConfigByKey("sys.account.initPasswordModify"));
         return initPasswordModify != null && initPasswordModify == 1 && pwdUpdateDate == null;
     }
 
     // 检查密码是否过期
-    public boolean passwordIsExpiration(Date pwdUpdateDate)
-    {
+    public boolean passwordIsExpiration(Date pwdUpdateDate) {
         Integer passwordValidateDays = Convert.toInt(configService.selectConfigByKey("sys.account.passwordValidateDays"));
-        if (passwordValidateDays != null && passwordValidateDays > 0)
-        {
-            if (StringUtils.isNull(pwdUpdateDate))
-            {
+        if (passwordValidateDays != null && passwordValidateDays > 0) {
+            if (StringUtils.isNull(pwdUpdateDate)) {
                 // 如果从未修改过初始密码，直接提醒过期
                 return true;
             }
